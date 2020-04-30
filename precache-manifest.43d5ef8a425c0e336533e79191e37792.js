69cc2235ce95b01f763def8746ec50ffe7e8c617
FilipeMts <jfgmatos@outlook.com>
2020-04-30 10:46:01 +0100

    Updates

---
+++ precache-manifest.43d5ef8a425c0e336533e79191e37792.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de47b9cefde60765f119b0c462358f9b",
+    "url": "/FCC-Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "1a272cd18ed103804ac4",
+    "url": "/FCC-Markdown-Previewer/static/css/main.01fdd662.chunk.css"
+  },
+  {
+    "revision": "49085b62e01a98976707",
+    "url": "/FCC-Markdown-Previewer/static/js/2.0cbeafe5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FCC-Markdown-Previewer/static/js/2.0cbeafe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a272cd18ed103804ac4",
+    "url": "/FCC-Markdown-Previewer/static/js/main.90f692e8.chunk.js"
+  },
+  {
+    "revision": "ed2495094db6f5a36b6c",
+    "url": "/FCC-Markdown-Previewer/static/js/runtime-main.c56c1a49.js"
+  }
+]);
